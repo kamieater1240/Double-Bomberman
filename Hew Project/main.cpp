c9bf46ebc722bfaba4b0bc9bf03791666e931e27
kamieater1240 <joshdragon821019@yahoo.com.tw>
2019-03-01 04:25:18 +0900

    Project Created

---
+++ Hew Project/main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "main.h"
+
+HANDLE hWindow = GetStdHandle(STD_OUTPUT_HANDLE);
+
+int main() {
+
+	bool endGame = false;
+	while (!endGame) {
+
+		SetConsoleTitle("DoubleBombs!");
+
+	}
+
+	rewind(stdin);
+	getchar();
+	return 0;
+}
---
+++ Hew Project/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <windows.h>
+#include <mmsystem.h>
+
+#define INDEXSIZE		256
+#define MAPWIDTH		256
+#define MAPHEIGHT		128
+#define DECODELAYER		8
+#define MAPLAYER		4
+#define SCREENWIDTH		128
+#define SCREENHEIGHT	64
+#define MAPCOUNT		128
+#define PIXELSIZE		8
+#define TILEX			8
+#define TILEY			8
